0227b0a62d957a2988a793415604e3dabcb47e5c
Joshua Joel <joeljoshua460@gmail.com>
2026-05-01 08:59:56 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -32,9 +32,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import type { ClientRequest, IncomingMessage } from "http";
-// import type { ProxyServer } from "http-proxy";
 
-// https://vite.dev/config/
 export default defineConfig(async () => {
   const plugins = [react(), tailwindcss()];
   try {
